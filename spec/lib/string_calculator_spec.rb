fb529d05aad28ccacecdaa30889f0409226c6ceb
Chandrakant <kohadchandrakant@gmail.com>
2025-11-17 07:53:32 +0000

    test: add failing test for empty string (TDD - red)

---
+++ spec/lib/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+# spec/lib/string_calculator_spec.rb
+require "rails_helper"
+require "string_calculator"
+
+RSpec.describe StringCalculator do
+  describe ".add" do
+    it "returns 0 for an empty string" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+  end
+end
