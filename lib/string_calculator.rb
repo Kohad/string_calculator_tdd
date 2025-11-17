8b192cff7536c12542a98e684425741e4babfd77
Chandrakant <kohadchandrakant@gmail.com>
2025-11-17 07:59:16 +0000

    test: add single number case (TDD - red); feat: parse single number (green)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,4 +3,9 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.nil? || numbers.empty?
   end
+
+  def self.add(numbers)
+    return 0 if numbers.nil? || numbers.empty?
+    return numbers.to_i if numbers.match?(/\A-?\d+\z/)
+  end
 end
